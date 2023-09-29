a5f0e41a4874a6ce1331716bd3b99d8aaee37b62
muhammedgunaydin <muhammed-gunaydin@hotmail.com>
2023-09-29 19:07:46 +0300

    :sparkles: add mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import helmet from 'helmet'
 import cors from 'cors'
+import bookRouter from '../src/routes/bookRouter'
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/bookDB').then(() => {
+  console.log('MongoDB connected')
+})
 
 //middlewares
 app.use(express.json())
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('hello')
-})
+app.use('/api/book', bookRouter)
 
 const port = 3000
 app.listen(port, () => {
