78c74003e7fb0bccbdb274b68099ad20a4768e7e
muhammedgunaydin <muhammed-gunaydin@hotmail.com>
2023-09-29 22:03:28 +0300

    :sparkles: add other controllers and routes

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -14,3 +14,57 @@ export const createBook = async (req: Request, res: Response) => {
     }
   }
 }
+
+export const getAllBooks = async (req: Request, res: Response) => {
+  try {
+    const book = await Book.find()
+    res.json(book)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message })
+    } else {
+      res.status(500).json({ error: 'Unknown error!' })
+    }
+  }
+}
+
+export const getBookById = async (req: Request, res: Response) => {
+  try {
+    const book = await Book.findById(req.params.id)
+    res.json(book)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message })
+    } else {
+      res.status(500).json({ error: 'Unknown error!' })
+    }
+  }
+}
+
+export const updateBook = async (req: Request, res: Response) => {
+  try {
+    const uptatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+    res.json(uptatedBook)
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message })
+    } else {
+      res.status(500).json({ error: 'Unknown error!' })
+    }
+  }
+}
+
+export const deleteBook = async (req: Request, res: Response) => {
+  try {
+    await Book.findByIdAndRemove(req.params.id)
+    res.json({ msg: 'Book deleted succesfully' })
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json({ error: err.message })
+    } else {
+      res.status(500).json({ error: 'Unknown error!' })
+    }
+  }
+}
--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -1,8 +1,18 @@
 import express from 'express'
-import { createBook } from '../controllers/bookController'
+import {
+  createBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
+} from '../controllers/bookController'
 
 const router = express.Router()
 
-router.post("/",createBook)
+router.post('/', createBook)
+router.get('/', getAllBooks)
+router.get('/:id', getBookById)
+router.put('/:id', updateBook)
+router.delete('/:id', deleteBook)
 
 export default router
